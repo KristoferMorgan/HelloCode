e0987a44f2fa9e6735fdd910f0ee01d039913dea
Christopher <rezielsaw@gmail.com>
2022-04-28 17:09:32 +1000

    Example005_Random Project

---
+++ example005_Random/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти сумму двух чисел с случайными значениями
+int Number_A = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(Number_A);
+int Number_B = new Random().Next(1, 10); // 1 2 3 4....9
+Console.WriteLine(Number_B);
+int result = Number_A + Number_B;
+Console.WriteLine(result);
